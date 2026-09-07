ba6c23225e55293daeccdb85595692897662feac
Akshay Rane <akshayrane2750@gmail.com>
2026-09-07 22:17:09 +0530

    Added required methods in service

--- User/src/main/java/com/user/service/UserService.java
+++ User/src/main/java/com/user/service/UserService.java
@@ -1,5 +1,20 @@
 package com.user.service;
 
+import java.util.List;
+
+import com.user.dto.UserCreateDto;
+import com.user.dto.UserDto;
+
 public interface UserService {
+
+	UserDto createUser(UserCreateDto userCreateDto);
+
+	UserDto getUserById(Long id);
+
+	List<UserDto> getAllUsers();
+
+	UserDto updateUser(UserDto userDto, Long id);
+
+	void deleteUser(Long id);
 
 }
--- User/src/main/java/com/user/service/UserServiceImpl.java
+++ User/src/main/java/com/user/service/UserServiceImpl.java
@@ -1,5 +1,62 @@
 package com.user.service;
 
-public class UserServiceImpl implements UserService{
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
+import com.user.dto.UserCreateDto;
+import com.user.dto.UserDto;
+import com.user.entity.User;
+import com.user.mapper.UserMapper;
+import com.user.repository.UserRepository;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	private UserRepository userRepository;
+
+	public UserServiceImpl(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+
+	@Override
+	public UserDto createUser(UserCreateDto userCreateDto) {
+		User user = UserMapper.mapToUser(userCreateDto);
+		User saveUser = userRepository.save(user);
+		return UserMapper.mapToUserDto(saveUser);
+	}
+
+	@Override
+	public UserDto getUserById(Long id) {
+		User user = userRepository.findById(id).orElseThrow(() -> new RuntimeException("user does not exits"));
+		return UserMapper.mapToUserDto(user);
+	}
+
+	@Override
+	public List<UserDto> getAllUsers() {
+		List<User> users = userRepository.findAll();
+		return users.stream().map((user) -> UserMapper.mapToUserDto(user)).collect(Collectors.toList());
+	}
+
+	@Override
+	public UserDto updateUser(UserDto userDto, Long id) {
+		User user = userRepository.findById(id).orElseThrow(() -> new RuntimeException("User does not exist"));
+
+		user.setName(userDto.name());
+		user.setEmail(userDto.email());
+		user.setPhone(userDto.phone());
+
+		User updatedUser = userRepository.save(user);
+		return UserMapper.mapToUserDto(updatedUser);
+	}
+
+	@Override
+	public void deleteUser(Long id) {
+		if (!userRepository.existsById(id)) {
+			throw new RuntimeException("User does not exist");
+		}
+		userRepository.deleteById(id);
+	}
 
 }
