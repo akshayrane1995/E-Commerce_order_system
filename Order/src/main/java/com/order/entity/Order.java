e89609d5ef3e4a59dcc5e9023590f5144a91568e
Akshay Rane <akshayrane2750@gmail.com>
2026-09-15 23:45:39 +0530

    Implemented Order entity

---
+++ Order/src/main/java/com/order/entity/Order.java
@@ -0,0 +1,44 @@
+package com.order.entity;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.order.constant.OrderStatus;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "order_details")
+public class Order {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private Long userId;
+	private BigDecimal totalAmount;
+	
+	@Enumerated(EnumType.STRING)
+	private OrderStatus status;
+	private LocalDateTime createdAt;
+	private LocalDateTime updatedAt;
+	
+	@OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<OrderItem> item; 
+}
