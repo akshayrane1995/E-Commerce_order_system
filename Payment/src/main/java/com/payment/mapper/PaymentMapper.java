2fe1a5fc6171222ee6729d92a7160e6fdf1e9451
Akshay Rane <akshayrane2750@gmail.com>
2026-09-21 16:28:15 +0530

    Implement paymentmapper

---
+++ Payment/src/main/java/com/payment/mapper/PaymentMapper.java
@@ -0,0 +1,31 @@
+package com.payment.mapper;
+
+import com.payment.dto.PaymentDto;
+import com.payment.entity.Payment;
+
+public class PaymentMapper {
+
+	public static Payment mapToPayment(PaymentDto paymentDto){
+		
+		Payment payment = new Payment(
+				null,
+				paymentDto.orderID(),
+				paymentDto.amount(),
+				paymentDto.status(),
+				null,
+				null);
+		return payment;
+	}
+	
+	public static PaymentDto mapToPaymentDto(Payment payment){
+		
+		PaymentDto paymentDto = new PaymentDto(
+				payment.getId(),
+				payment.getOrderId(),
+				payment.getAmount(),
+				payment.getStatus(),
+				payment.getCreatedAt(),
+				payment.getUpdatedAt());
+		return paymentDto;
+	}
+}
