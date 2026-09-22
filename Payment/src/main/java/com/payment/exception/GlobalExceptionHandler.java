b24e12aae070d02de638e9548bdd60de1d091ad1
Akshay Rane <akshayrane2750@gmail.com>
2026-09-22 16:31:46 +0530

    Added global exception handler

---
+++ Payment/src/main/java/com/payment/exception/GlobalExceptionHandler.java
@@ -0,0 +1,52 @@
+package com.payment.exception;
+
+import java.time.LocalDateTime;
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<ErrorDetails> handleResourceNotFoundException(ResourceNotFoundException exception , WebRequest webRequest){
+		
+		ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), exception.getMessage(),
+									webRequest.getDescription(false), "PAYMENT_RESOURCE_NOT_FOUND");
+		
+		return new ResponseEntity<>(errorDetails , HttpStatus.NOT_FOUND);
+	}
+	
+	//handle generic exception
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<ErrorDetails> handleGenericException(Exception exception, WebRequest webRequest) {
+		ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), exception.getMessage(),
+				webRequest.getDescription(false), "Internal Server error");
+
+		return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ErrorDetails> handleValidationException(MethodArgumentNotValidException exception,
+			WebRequest webRequest) {
+
+		String message = exception.getBindingResult()
+				.getFieldErrors()
+				.stream()
+				.map(error -> error.getField() + ": " + error.getDefaultMessage())
+				.collect(Collectors.joining(", "));
+
+		ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), message, webRequest.getDescription(false),
+				"VALIDATION_ERROR");
+
+		return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+	}
+
+	
+}
