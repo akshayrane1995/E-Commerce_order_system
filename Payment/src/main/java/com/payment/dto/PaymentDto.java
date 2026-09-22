c4265b8bbe4491b24a255a611e2ffc78e4bfa404
Akshay Rane <akshayrane2750@gmail.com>
2026-09-22 16:25:01 +0530

    Added validation

--- Payment/src/main/java/com/payment/dto/PaymentDto.java
+++ Payment/src/main/java/com/payment/dto/PaymentDto.java
@@ -1,11 +1,27 @@
 package com.payment.dto;
 
 import java.math.BigDecimal;
+
 import java.time.LocalDateTime;
 
-import com.payment.constant.PaymentStauts;
+import com.payment.constant.PaymentStatus;
 
-public record PaymentDto(Long id, Long orderID, BigDecimal amount, PaymentStauts status,
-						LocalDateTime createdAt, LocalDateTime updatedAt) {
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+
+public record PaymentDto(
+					Long id, 
+					
+					@NotNull(message = "Order id is required")
+					Long orderId, 
+					
+					@NotNull(message = "Amount is required")
+			        @DecimalMin(value = "0.0", inclusive = false, message = "Amount must be greater than 0")
+					BigDecimal amount, 
+					
+					
+					PaymentStatus status,
+					LocalDateTime createdAt, 
+					LocalDateTime updatedAt) {
 
 }
